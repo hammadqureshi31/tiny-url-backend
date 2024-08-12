25460a0a783522358f89e05cecdd2bdfa052958f
hammadqureshi31 <154003051+hammadqureshi31@users.noreply.github.com>
2024-08-13 03:09:53 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 export const port = process.env.PORT || 3000
 
-export const mongodb_url = process.env.MONGODB_URL || "mongodb://localhost:27017/tinyURL"
+export const mongodb_url = process.env.MONGODB_URL || "mongodb+srv://hammadqureshi:Hammad_qureshi-190@cluster0.cjcec1p.mongodb.net/tinyURL?retryWrites=true&w=majority&appName=Cluster0"
 
 export const accessTokenSecret = process.env.ACCESS_TOKEN_SECRET || "tiny-url";
 export const accessTokenExpiry = process.env.ACCESS_TOKEN_EXPIRY || "1d";
@@ -8,5 +8,4 @@ export const refreshTokenSecret = process.env.REFRESH_TOKEN_SECRET || "tiny-url-
 export const refreshTokenExpiry = process.env.REFRESH_TOKEN_EXPIRY || "10d";
 
 export const googleClientId = process.env.GOOGLE_CLIENT_ID 
-
 export const googleClientSecret = process.env.GOOGLE_CLIENT_SECRET
